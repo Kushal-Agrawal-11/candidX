b0f7e408de144e4713f21aa0530fe8b216b7d8d0
harshudarade <hbdarade2018@gmail.com>
2023-08-06 16:01:33 +0530

    database connected

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}`);
+});
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Mongo connected ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`error ${error.message}`);
+    }
+}
+
+connectDB();
