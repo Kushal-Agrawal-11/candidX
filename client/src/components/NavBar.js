db2fc2ebdbcba26725d32667b74436129f13bdc7
Kushal <kushalagrawal2232@gmail.com>
2023-08-08 01:24:49 +0530

    Home Page Modified

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import styles from './Navbar.module.css'; // Import the CSS module
 
-const NavBar = () => {
+const Navbar = () => {
+  const location = useLocation();
+
   return (
-    <nav>
-      
+    <nav className={styles.nav}>
+      <Link to="/" className={styles.logo}>
+        <h1>candid<span>X</span></h1>
+      </Link>
+      <ul>
+        <li className={location.pathname === '/' ? styles.active : ''}><Link to="/">HOME</Link></li>
+        <li className={location.pathname === '/categories' ? styles.active : ''}><Link to="/categories">CATEGORIES</Link></li>
+        <li className={location.pathname === '/about' ? styles.active : ''}><Link to="/about">ABOUT</Link></li>
+        <li className={location.pathname === '/login' ? styles.active : ''}><Link to="/login">LOGIN</Link></li>
+        <li className={location.pathname === '/contact' ? styles.active : ''}><Link to="/contact">CONTACT</Link></li>
+      </ul>
+
+      <div className={styles['social-icons']}>
+        <a href="https://facebook.com"><i className={`fab fa-facebook-f ${styles.icon}`}></i></a>
+        <a href="https://twitter.com"><i className={`fab fa-twitter ${styles.icon}`}></i></a>
+        <a href="https://instagram.com"><i className={`fab fa-instagram ${styles.icon}`}></i></a>
+        <a href="https://pinterest.com"><i className={`fab fa-pinterest-p ${styles.icon}`}></i></a>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default Navbar;
--- client/src/screens/About.js
+++ client/src/screens/About.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from './About.module.css'; // Check the correct file path and name
+import Navbar from '../components/Navbar';
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className={styles['about-container']}>
+      <Navbar />
+
+      <h1>Hello Martin!</h1>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
